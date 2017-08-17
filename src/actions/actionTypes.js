8b71d64cd05f8cd2aa0946a535345b86b6f4e70f
fengwuyan <kergo000@163.com>
2017-08-17 20:43:53 +0800

    fengwuyan

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -17,17 +17,8 @@ export const SEARCH_HISTORY = 'SEARCH_HISTORY';
 //清空搜索历史
 export const CLEAR = 'CLEAR';
 //登陆
-export const Login = 'Login';//开始获取优惠券
-export const GET_COUPONS_DOING = 'GET_COUPONS_DOING';
-//获取优惠券完成
-export const GET_COUPONS_DONE = 'GET_COUPONS_DONE';<<<<<<< .mine
+export const Login = 'Login';
 //开始获取优惠券
 export const GET_COUPONS_DOING = 'GET_COUPONS_DOING';
 //获取优惠券完成
 export const GET_COUPONS_DONE = 'GET_COUPONS_DONE';
-=======
-//登陆
-export const Login = 'Login';
-
-
->>>>>>> .theirs
